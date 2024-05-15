5e5e893a0c3c7b21817cbcae236b3db67ee18fd1
Adwaith Shaju <adwaithshaju2003@gmail.com>
2024-05-15 22:07:52 +0530

    Initial commit

---
+++ Backend/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+  # audio_folder = "C:/Users/adwai/Desktop/Third Eye/Backend/audio_files"
+
+   
+    # if not os.path.exists(audio_folder):
+    #     os.makedirs(audio_folder)
+    # else:
+        
+    #     files = glob.glob(os.path.join(audio_folder, "*.mp3"))
+    #     for file in files:
+    #         os.remove(file)
+
+   
+    # audio_path = os.path.join(audio_folder, "sample.mp3")
+    # speech = gTTS(text=output, lang=language, slow=False)
+    # speech.save(audio_path)
